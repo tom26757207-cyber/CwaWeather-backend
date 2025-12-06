901e6226314f27dd706086048ac558c95414beee
tom26757207-cyber <tom26757207@gmail.com>
2025-12-06 21:56:23 +0800

    fix: 更新天氣 API 請求中的地點從宜蘭縣改為新北市

--- server.js
+++ server.js
@@ -37,18 +37,18 @@ const getKaohsiungWeather = async (req, res) => {
       {
         params: {
           Authorization: CWA_API_KEY,
-          locationName: "宜蘭縣",
+          locationName: "新北市",
         },
       }
     );
 
-    // 取得高雄市的天氣資料
+    // 取得新北市的天氣資料
     const locationData = response.data.records.location[0];
 
     if (!locationData) {
       return res.status(404).json({
         error: "查無資料",
-        message: "無法取得高雄市天氣資料",
+        message: "無法取得新北市天氣資料",
       });
     }
 
